a9672bb51d2bb16aa742fa7c8e992bcb9a0d7ccb
Dueynn Ogaz Siller <dueynn@gmail.com>
2018-06-28 12:05:21 -0500

    Refactor render of Dashboard so content doesn't repeat. Improve css of dashboard for mobile.

--- tienda-reactJS/client/app/components/Dashboard.jsx
+++ tienda-reactJS/client/app/components/Dashboard.jsx
@@ -51,72 +51,82 @@ class Dashboard extends React.Component {
     }
 
     render() {
-        if (this.state.details) {
-            console.log(this.state.detailProduct);
-            return (
-                <div className="container-fluid fill-height dashboard-container">
-                    <div className="container">
-                        <Navbar/>
-                        <div className="row no-margin-sides">
-                            <div className="col-12 top-content">
-                                <div className="row">
-                                    <div className="col-md-9 title-container">
-                                        <h1>{this.state.detailProduct.name}</h1>
-                                    </div>
+        let container = [];
+        let topContainer = [];
+        let topContainerContent = [];
+        if (this.state.details) { // Will show details view
+            // Add details title to topContainerContent
+            topContainerContent.push(
+                <div className="col-md-9 title-container" key="details_title">
+                    <h1>{this.state.detailProduct.name}</h1>
+                </div>
+            )
+            // Add details body to container
+            container.push (
+                    <div className="row no-margin-sides" key="details_container">
+                        <div className="col-12 details-content">
+                            <div className="row no-margin-sides">
+                                <div className="card col-md-5" id="card-details">
+                                    <img className="card-img-detail" src={'/assets/img/' + this.state.detailProduct.file} alt="Card image cap"/>
                                 </div>
-                                <hr/>
-                            </div>
-                        </div>
-                        <div className="row no-margin-sides">
-                            <div className="col-12 details-content">
-                                <div className="row no-margin-sides">
-                                    <div className="card col-md-5" id="card-details">
-                                        <img className="card-img-detail" src={'/assets/img/' + this.state.detailProduct.file} alt="Card image cap"/>
-                                    </div>
-                                    <div className="col-md-5" id="info-details">
-                                        <p className="card-text"><strong>Precio: </strong>${this.state.detailProduct.price}</p>
-                                        <p className="card-text"><strong>Unidades disponibles: </strong>{this.state.detailProduct.quantity}</p>
-                                    </div>
+                                <div className="col-md-5" id="info-details">
+                                    <p className="card-text"><strong>Precio: </strong>${this.state.detailProduct.price}</p>
+                                    <p className="card-text"><strong>Unidades disponibles: </strong>{this.state.detailProduct.quantity}</p>
                                 </div>
-                                <button className="btn btn-outline-secondary" id="showCatalog" onClick={e => this.hideDetails(e)}>Atrás</button>
                             </div>
+                            <button className="btn btn-outline-secondary" id="showCatalog" onClick={e => this.hideDetails(e)}>Atrás</button>
                         </div>
+                    </div>
+            )
+        } else { // Will show catalog view
+            // Add catalog title to topContainerContent
+            topContainerContent.push(
+                <div className="col-md-9 title-container" key="catalog_title">
+                    <h1>Catálogo de productos</h1>
+                </div>
+            )
+            // Add catalog search view to topContainerContent
+            topContainerContent.push(
+                <div className="col-md-3 search-container" key="catalog_search">
+                    <p>¿Qué estás buscando?</p>
+                    <div className="input-group">
+                        <div className="input-group-prepend">
+                            <span className="input-group-text" id="search_addon"><img src="/assets/img/ic_search.svg"/></span>
+                        </div>
+                        <input type="text" className="form-control" id="search" value={this.state.filter} onChange={e => this.onSearchChange(e)}/>
                     </div>
                 </div>
             )
-        } else {
-            return (
-                <div className="container-fluid fill-height dashboard-container">
-                    <div className="container">
-                        <Navbar/>
-                        <div className="row no-margin-sides">
-                            <div className="col-12 top-content">
-                                <div className="row">
-                                    <div className="col-md-9 title-container">
-                                        <h1>Catálogo de productos</h1>
-                                    </div>
-                                    <div className="col-md-3 search-container">
-                                        <p>¿Qué estás buscando?</p>
-                                        <div className="input-group">
-                                            <div className="input-group-prepend">
-                                                <span className="input-group-text" id="search_addon"><img src="/assets/img/ic_search.svg"/></span>
-                                            </div>
-                                            <input type="text" className="form-control" id="search" value={this.state.filter} onChange={e => this.onSearchChange(e)}/>
-                                        </div>
-                                    </div>
-                                </div>
-                                <hr/>
-                            </div>
-                        </div>
-                        <div className="row no-margin-sides">
-                            <div className="col-12 catalog-content">
-                                {this.createCatalog()}
-                            </div>
+            // Add catalog body to container
+            container.push (
+                    <div className="row no-margin-sides" key="catalog_container">
+                        <div className="col-12 catalog-content">
+                            {this.createCatalog()}
                         </div>
                     </div>
-                </div>
             )
         }
+        // Wrap topContainerContent and add to topContainer
+        topContainer.push(
+            <div className="row no-margin-sides" key="top_container">
+                <div className="col-12 top-content">
+                    <div className="row">
+                        {topContainerContent}
+                    </div>
+                    <hr/>
+                </div>
+            </div>
+        );
+        // Return assembled view in containerFluid and container, add Navbar, topContainer and container
+        return (
+            <div className="container-fluid fill-height dashboard-container">
+                <div className="container no-padding-sides">
+                    <Navbar shoppingCart={this.state.cart}/>
+                    {topContainer}
+                    {container}
+                </div>
+            </div>
+        )
     }
 
     componentWillMount() {
@@ -160,10 +170,10 @@ class Dashboard extends React.Component {
                         <p className="card-text"><strong>Precio: </strong>${this.state.products[product].price}</p>
                         <p className="card-text"><strong>Unidades disponibles: </strong>{this.state.products[product].quantity}</p>
                         <div className="row no-margin-sides card-bottom">
-                            <div className="col-sm-5 col-md-12 col-lg-5 no-padding-sides">
+                            <div className="col-5 col-md-12 col-lg-5 no-padding-sides">
                                 <button className="btn btn-primary" id={"details_" + this.removeDiacritics(this.state.products[product].name.toLowerCase())} onClick={e => this.showDetails(e)}>Ver más</button>
                             </div>
-                            <div className="col-sm-7 col-md-12 col-lg-7 no-padding-sides">
+                            <div className="col-7 col-md-12 col-lg-7 no-padding-sides">
                                 <div className="input-group">
                                     <div className="input-group-prepend">
                                         <button className="btn btn-secondary" type="button" id={"count_" + this.removeDiacritics(this.state.products[product].name.toLowerCase())}>Añadir</button>
