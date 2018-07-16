ae7b1ce65fc55e465fca1213fea04f636a7f8832
Dueynn Ogaz Siller <dueynn@gmail.com>
2018-07-16 18:21:11 -0500

    Add cart html template. Load cart from local storage on constructor. Add cart css. Finish dynamic cart view and cancel functionality.

--- tienda-Angular2/src/app/cart/cart.component.ts
+++ tienda-Angular2/src/app/cart/cart.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from '../services/cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['../dashboard/dashboard.component.css','./cart.component.css']
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+  shoppingCart = {};
+
+  constructor(private cartService: CartService) {
+      this.shoppingCart = cartService.getCart();
+  }
 
   ngOnInit() {
+  }
+
+  getTotal(){
+      let total = 0;
+      for (let item in this.shoppingCart){
+          total += this.shoppingCart[item].count*this.shoppingCart[item].price;
+      }
+      return total;
+  }
+
+  cancel() {
+      this.shoppingCart = {};
+      this.cartService.setCart({});
   }
 
 }
